3516db64c477e504a0d20c432f4c7382affb5e9b
Jozsef Mozer <mozerj2001@gmail.com>
2023-05-15 18:31:50 +0200

    Vector concatenator first working version.

--- fp_accel.srcs/sim_3/new/tb_vec_cat.v
+++ fp_accel.srcs/sim_3/new/tb_vec_cat.v
@@ -7,18 +7,23 @@ module tb_vec_cat(
 
     localparam BUS_WIDTH = 20;
     localparam VECT_WIDTH = 35;
+    localparam CAT_REG_NO = 4;
+
     localparam CLK_PERIOD = 10;
+    localparam HALF_CLK_PERIOD = CLK_PERIOD/2;
+
     reg clk = 1'b0;
     reg rst = 1'b1;
     reg [BUS_WIDTH-1:0] vector;
-    reg valid = 1'b1;
+    reg valid = 1'b0;
     wire [BUS_WIDTH-1:0] cat_vector;
     wire cat_valid;
 
     vec_cat
     #(
         .BUS_WIDTH(BUS_WIDTH),
-        .VECT_WIDTH(VECT_WIDTH)
+        .VECT_WIDTH(VECT_WIDTH),
+        .CAT_REG_NO(CAT_REG_NO)
     ) uut(
     .clk(clk),
     .rst(rst),
@@ -30,11 +35,12 @@ module tb_vec_cat(
 
     always begin 
         clk <= ~clk;
-        #CLK_PERIOD/2;
+        #HALF_CLK_PERIOD;
     end
 
     initial begin
         #50;
+        valid <= 1'b1;
         rst <= 1'b0;
         vector <= 20'b11111111111111111111;
         #CLK_PERIOD;
@@ -51,6 +57,8 @@ module tb_vec_cat(
         vector <= 20'b10101010101010101010;
         #CLK_PERIOD;
         vector <= 20'b11111111111111111111;
+        #CLK_PERIOD;
+        valid <= 1'b0;
     end
 
 
