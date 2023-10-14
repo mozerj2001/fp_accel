f8fc6355034c9e1165335f076103d01f93b30b88
Jozsef Mozer <jozmoz01@e128381.arm.com>
2023-10-14 12:45:29 +0200

    CNT Comparator module created.
    Created comparator.v, a module which performs
    CNT(A)+CNT(B)-CNT(C), then uses the result as an
    address into a block RAM, that stores 0s and 1s.
    The RAM output codes whether a certain sum
    is over or under the desired threshold.
    
    ++ block RAM module added, that was needed for the
    comparator.

---
+++ project_1.srcs/sources_1/new/comparator.v
@@ -0,0 +1,64 @@
+`timescale 1ns / 1ps
+`default_nettype none
+
+`include "block_ram_rd_1st.v"
+
+// COMPARATOR
+// Calculates CNT(A)+CNT(B)-CNT(C) to provide a (dis)similarity
+// metric between A and B vectors, if C = A & B;.
+// The result is used as an address into a 1 bit wide block RAM,
+// that contains 1s up until the desired threshold address, then 0s.
+
+module comparator
+    #(
+        VECTOR_WIDTH    = 920,
+        //
+        CNT_WIDTH       = $clog2(VECTOR_WIDTH),
+        ADDR_WIDTH      = CNT_WIDTH
+    )(
+        input wire                  clk,
+        input wire                  rst,
+        input wire [CNT_WIDTH-1:0]  i_CntA,
+        input wire [CNT_WIDTH-1:0]  i_CntB,
+        input wire [CNT_WIDTH-1:0]  i_CntC,
+        input wire                  i_RAM_Setup,    // Mux i_Addr onto the addr input of the RAM module
+
+        // RAM I/O
+        input wire [ADDR_WIDTH-1:0] i_Addr,
+        input wire                  i_Din,
+        input wire                  i_WrEn,
+        output wire                 o_Dout          // 0: over threshold, 1: under threshold
+        
+    );
+
+    // Similarity calc
+    reg [CNT_WIDTH-1:0] r_Sum;
+
+    always @ (posedge clk)
+    begin
+        if(rst) begin
+            r_Sum <= 0;
+        end else begin
+            r_Sum <= i_CntA + i_CntB - i_CntC;
+        end
+    end
+
+    // RAM storing Y/N values based on the configured threshold
+    wire [ADDR_WIDTH-1:0] w_Addr;
+    assign w_Addr = i_RAM_Setup ? i_Addr : r_Sum;
+
+    block_ram_rd_1st
+    #(
+        .DEPTH  (VECTOR_WIDTH+1),
+        .WIDH   (1)
+    ) u_result_ram (
+        .clk    (clk),
+        .we     (i_WrEn),
+        .en     (1),
+        .addr   (w_Addr),
+        .din    (i_Din),
+        .dout   (o_Dout)
+    );
+
+
+endmodule
---
+++ project_1.srcs/sources_1/new/comparator_wrapper.v
@@ -0,0 +1,107 @@
+`timescale 1ns / 1ps
+`default_nettype none
+
+// COMPARATOR_WRAPPER
+// This module is a wrapper to the comparator module in comparator.v.
+// Its purpose is to hide RAM setup from external hardware. The only
+// input needed are the desired threshold of (dis)similarity (i_Threshold)
+// and the write signal for the threshold (i_WrThreshold). These need to be
+// provided in the same clk.
+
+
+module comparator_wrapper
+    #(
+        VECTOR_WIDTH    = 920,
+        BUS_WIDTH       = 512,
+        //
+        CNT_WIDTH       = $clog2(VECTOR_WIDTH),
+        ADDR_WIDTH      = CNT_WIDTH
+    )(
+        input wire                  clk,
+        input wire                  rst,
+        input wire [CNT_WIDTH-1:0]  i_CntA,
+        input wire [CNT_WIDTH-1:0]  i_CntB,
+        input wire [CNT_WIDTH-1:0]  i_CntC,
+        input wire                  i_WrThreshold,  // Mux i_Addr onto the addr input of the RAM module
+        input wire [CNT_WIDTH-1:0]  i_Threshold,
+        output wire                 o_Ready,
+        output wire                 o_Dout          // 1: over threshold, 0: under threshold
+    );
+
+    localparam LOAD_RAM_0   = 2'b00;
+    localparam LOAD_RAM_1   = 2'b01;
+    localparam COMPARE      = 2'b10;
+
+    // THRESHOLD REG
+    // Register in which the threshold is stored when i_WrThreshold is
+    // asserted.
+    reg [CNT_WIDTH-1:0] r_Threshold;
+
+    always @ (posedge clk)
+    begin
+        if(i_WrThreshold) begin
+            r_Threshold <= i_Threshold;
+        end
+    end
+
+
+    // STATE MACHINE
+    // LOAD RAM: Load RAM with an address counter. (din: 1s or 0s)
+    // COMPARE: Calculate CntA+CntB-CntC. o_Dout is 1 when under
+    // the threshold, 0 when over the threshold.
+    reg [1:0] r_State;
+    reg [CNT_WIDTH-1:0] r_AddrCntr;
+
+    always @ (posedge clk)
+    begin
+        if(rst) begin
+            r_State <= COMPARE;
+        end else if((r_State == COMPARE) && i_WrThreshold) begin
+            r_State <= LOAD_RAM_0;
+        end else if(r_AddrCntr == VECTOR_WIDTH) begin
+            r_State <= COMPARE;
+        end else if(r_AddrCntr == r_Threshold) begin
+            r_State <= LOAD_RAM_1;
+        end
+    end
+
+    // ADDRESS COUNTER
+    always @ (posedge clk)
+    begin
+        if(rst) begin
+            r_AddrCntr <= 0;
+        end else if(i_WrThreshold && (r_State == COMPARE)) begin
+            r_AddrCntr <= 0;
+        end else if(r_State != COMPARE) begin
+            r_AddrCntr <= r_AddrCntr + 1;
+        end
+    end
+
+    // COMPARATOR
+    wire w_CompDin;
+    assign w_CompDin = (r_State == LOAD_RAM_0) ? 1'b1 : 1'b0;
+
+    comparator #(
+        .VECTOR_WIDTH   (VECTOR_WIDTH),
+        .BUS_WIDTH      (BUS_WIDTH)
+    ) u_comparator (
+        .clk            (clk),
+        .rst            (rst),
+        .i_CntA         (i_CntA),
+        .i_CntB         (i_CntB),
+        .i_CntC         (i_CntC),
+        .i_RAM_Setup    (r_State != COMPARE),
+        .i_Addr         (r_AddrCntr),
+        .i_Din          (w_CompDin),
+        .i_WrEn         (r_State != COMPARE),
+        .o_Dout         (o_Dout)
+    );
+
+
+    assign o_Ready = (r_State == COMPARE);
+
+
+endmodule
+
+
+
