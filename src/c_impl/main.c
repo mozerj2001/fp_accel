454b1776fa0ada2b8c0cf01e5498c57fd06c6ccb
Jozsef Mozer <mozerj2001@gmail.com>
2025-02-22 13:10:13 +0100

    C implementation redone, reference results gen

--- src/c_impl/main.c
+++ src/c_impl/main.c
@@ -1,19 +1,40 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdint.h>
-#include <string.h>
-#include <math.h>
 #include "tanimoto.h"
 
-int main(void){
+int main(void)
+{
+    // Create random vectors
+    initVectors();
 
-    unsigned int i, j;
-    unsigned int pairCnt;
-    unsigned int idPairArr[REF_VEC_NO*CMP_VEC_NO][2];
-    FINGERPRINT A[REF_VEC_NO];
-    FINGERPRINT B[CMP_VEC_NO];
+    // Export random vectors to binary file, to be read by the accelerator OCL kernel
+    if (writeVectorsToFile("vectors.bin") != 0) {
+        fprintf(stderr, "Error writing vectors to file.\n");
+        return 1;
+    }
 
-    gen_test_set(A, REF_VEC_NO, B, CMP_VEC_NO, "ref_vec.txt", "cmp_vec.txt");
+    // Calculate C = A & B vectors
+    createIntermediaryVectors();
+
+    // Calculate CNT(1) for each vector
+    for (int i = 0; i < REF_VECTOR_NO; i++) {
+        calculateBinaryWeight(&referenceVectors[i]);
+    }
+    for (int i = 0; i < CMP_VECTOR_NO; i++) {
+        calculateBinaryWeight(&comparisonVectors[i]);
+    }
+    for (int i = 0; i < REF_VECTOR_NO * CMP_VECTOR_NO; i++) {
+        calculateBinaryWeight(&intermediaryVectors[i]);
+    }
+
+    // Calculate all Tanimoto similarity coefficients for each ref-cmp pair,
+    // save results in the tanimotoResults[] array
+    computeAllTanimotoSimilarities();
+
+    // Take a look at some of the results locally
+    for(int i = 0; i < REF_VECTOR_NO * CMP_VECTOR_NO; i++) {
+        printResult(tanimotoResults[i], THRESHOLD);
+    }
+
+    printAllResultsToTxtFile("results.txt");
 
     return 0;
 }
