5b5b0a7af4f17f249c8d11a46e35cd391a1d742e
Jozsef Mozer <mozerj2001@gmail.com>
2025-03-16 18:05:51 +0100

    HLS AXI burst

--- src/hls_dma/hls_dma.h
+++ src/hls_dma/hls_dma.h
@@ -10,6 +10,7 @@
 #define BUS_WIDTH 512
 #define VEC_ID_WIDTH 8
 #define REF_VEC_NO 8							// how many ref_vecs can be pushed before the comparison vectors (SHR_DEPTH)
+#define AXI_BURST_LENGTH 16
 
 typedef ap_uint<BUS_WIDTH> 		bus_t;			// 512 bit wide data word
 typedef ap_uint<VEC_ID_WIDTH>	vec_no_t;		// number of CMP vectors, corresponds to VEC_ID_WIDTH and the corresponding handshake interface data width
@@ -17,8 +18,10 @@ typedef ap_uint<VEC_ID_WIDTH*2> id_pair_t;		// pair of output vector IDs
 
 // AXI Stream lib types for interfaces
 typedef hls::axis<bus_t, 0, 0, 0> 		axis_vec_t;
+typedef hls::axis<vec_no_t, 0, 0, 0>	axis_vec_no_t;
 typedef hls::axis<id_pair_t, 0, 0, 0> 	axis_id_pair_t;
 typedef hls::stream<axis_vec_t> 		axi_stream_vec_t;
+typedef hls::stream<axis_vec_no_t>		axi_stream_vec_no_t;
 typedef hls::stream<axis_id_pair_t> 	axi_stream_id_pair_t;
 
 
@@ -30,12 +33,15 @@ void vec_intf(  bus_t*              vec_ref,
 void id_intf(   axi_stream_id_pair_t& 	id_in,
                 id_pair_t*              id_out);
 
+void vec_no_intf(	vec_no_t 			cmp_vec_no_in,
+				axi_stream_vec_no_t& 	cmp_vec_no_out);
+
 extern "C" void hls_dma(  	bus_t*                  vec_ref,
 							bus_t*                  vec_cmp,
 							axi_stream_vec_t&     	vec_out,
 							axi_stream_id_pair_t& 	id_in,
 							id_pair_t* 				id_out,
 							vec_no_t				cmp_vec_no_in,
-							vec_no_t				cmp_vec_no_out);
+							axi_stream_vec_no_t&	cmp_vec_no_out);
 
 #endif
