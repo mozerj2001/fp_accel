f5d5e9ea52a56fc95675e203c495713ccda2fef5
Jozsef Mozer <mozerj2001@gmail.com>
2025-03-29 09:55:16 +0100

    [VEC_CAT] ready + bus_width change
    i_Ready added, vec_cat is paused when
    its value is not 1. This will enable
    pausing the pipeline when a FIFO is full.
    
    BUS_WIDTH and SUB_VEC_NO are now
    proper parameters. The module will
    theoretically work with any legal
    combination.

--- src/verilog/sources_1/vec_cat.v
+++ src/verilog/sources_1/vec_cat.v
@@ -5,22 +5,23 @@
 `default_nettype none
 
 
-// VEC_CAT
-// To the input of the accelerator, vectors will arrive one after another,
-// without being separated from each other into different data words. The
-// purpose of this module is to do this separation. As the vector width is
-// different from the bus width, the last piece of each vector will be padded
-// with zeros on the output. (Zeroes don't influence the result of counting
-// 1 bits.)
-//
-// [ASSUMPTION]: The vector width is never more than twice the width of the
-// input bus.
+/* 
+ * MODULE: VEC_CAT
+ * To the input of the accelerator, vectors will arrive one after another,
+ * without being separated from each other into different data words. The
+ * purpose of this module is to do this separation. As the vector width is
+ * different from the bus width, the last piece of each vector will be padded
+ * with zeros on the output. (Zeroes don't influence the result of counting
+ * 1 bits.)
+ */
+
 module vec_cat
     #(
-        BUS_WIDTH       = 512,
+        BUS_WIDTH       = 128,
         VECTOR_WIDTH    = 920,
         VEC_ID_WIDTH    = 8,
-        REF_VECTOR_NO   = 8
+        //
+        SUB_VEC_NO      = $ceil($itor(VECTOR_WIDTH)/$itor(BUS_WIDTH))
     )
     (
         input wire                      clk,
@@ -36,34 +37,38 @@ module vec_cat
         output wire [BUS_WIDTH-1:0]     o_Vector,       // stream of separated vectors, only one vector per output word
         output wire [VEC_ID_WIDTH-1:0]  o_VecID,
         output wire                     o_Valid,        // o_Vector is valid
-        output wire                     o_Last          // last subvector un current compare batch
+        output wire                     o_Last,         // last subvector un current compare batch
+        input wire                      i_Ready         // read next sub-vector
 
     );
 
-    localparam CAT_REG_NO		    = 2                                 ;   // min. 2
-    localparam DELTA 			    = 2*BUS_WIDTH - VECTOR_WIDTH        ;   // step distance in each iterateion
-    localparam IDX_REG_WIDTH        = $clog2((CAT_REG_NO-1)*BUS_WIDTH)+1;
-    localparam FULL                 = 0                                 ;
-    localparam PAD                  = 1                                 ;
+    localparam CAT_REG_NO		    = 2                                  ;  // min. 2
+    localparam DELTA 			    = $rtoi(SUB_VEC_NO*BUS_WIDTH - VECTOR_WIDTH);  // step distance in each iterateion
+    localparam IDX_REG_WIDTH        = $clog2((CAT_REG_NO-1)*BUS_WIDTH)+1 ;
+    localparam FULL                 = 0                                  ;
+    localparam PAD                  = 1                                  ;
     
+    // internal valid signal
+    wire w_DoShift;
+    assign w_DoShift = i_Valid && i_Ready;
 
     /////////////////////////////////////////////////////////////////////////////////////
     // VECTOR SHIFT --> store current and previous CAT_REG_NO number of input vectors
-    reg [CAT_REG_NO*BUS_WIDTH-1:0] r_InnerVector;
-    wire                    w_Overflow;
+    reg [CAT_REG_NO*BUS_WIDTH-1:0]  r_InnerVector;
+    wire                            w_Overflow;
 
     genvar ii;
     generate
         for(ii = 1; ii <= CAT_REG_NO; ii = ii + 1) begin
             if(ii == 1) begin
                 always @ (posedge clk) begin
-                    if(i_Valid && ~w_Overflow) begin
+                    if(w_DoShift && ~w_Overflow) begin
                         r_InnerVector[BUS_WIDTH-1:0] <= i_Vector;
                     end
                 end
             end else begin
                 always @ (posedge clk) begin
-                    if(i_Valid && ~w_Overflow) begin
+                    if(w_DoShift && ~w_Overflow) begin
                         r_InnerVector[ii*BUS_WIDTH-1:(ii-1)*BUS_WIDTH] <= r_InnerVector[(ii-1)*BUS_WIDTH-1:(ii-2)*BUS_WIDTH];
                     end
                 end
@@ -83,7 +88,7 @@ module vec_cat
         if(!rstn) begin
             r_ValidShr <= 0;
             r_LastShr <= 0;
-        end else begin
+        end else if(i_Ready) begin
             r_ValidShr[0] <= i_Valid;
             r_ValidShr[1] <= r_ValidShr[0];
             r_ValidShr[2] <= r_ValidShr[1];
@@ -110,26 +115,31 @@ module vec_cat
 
     /////////////////////////////////////////////////////////////////////////////////////
     // CREATE OUTPUT
-    reg [IDX_REG_WIDTH-1:0] r_IdxReg;
-    reg                     r_State;
-    assign w_Overflow = ((r_IdxReg + DELTA) > (CAT_REG_NO-1)*BUS_WIDTH) && (r_State == PAD);    // 1, when information from the next vector would be shifted out
+    reg [IDX_REG_WIDTH-1:0]         r_IdxReg;
+    wire                            w_State;
+    reg [$clog2(SUB_VEC_NO)-1:0]    r_SubVecCntr;
+    assign w_Overflow = ((r_IdxReg + DELTA) > (CAT_REG_NO-1)*BUS_WIDTH) && (w_State == PAD);    // 1, when information from the next vector would be shifted out
 
     always @ (posedge clk)
     begin
         if(!rstn) begin
-            r_State <= PAD;
-        end else if(i_Valid) begin
-            r_State <= ~r_State;
+            r_SubVecCntr <= 0;
+        end else if(w_DoShift && (w_State == PAD)) begin
+            r_SubVecCntr <= 0;
+        end else if(w_DoShift) begin
+            r_SubVecCntr <= r_SubVecCntr + 1;
         end
     end
+
+    assign w_State = (r_SubVecCntr == (SUB_VEC_NO-1)) ? PAD : FULL;
 
     always @ (posedge clk)
     begin
         if(!rstn) begin
             r_IdxReg = 0;
-        end else if(r_State == PAD && ~w_Overflow && r_ValidShr[1]) begin
+        end else if(w_State == PAD && ~w_Overflow && r_ValidShr[1] && i_Ready) begin
             r_IdxReg = r_IdxReg + DELTA;
-        end else if(w_Overflow) begin
+        end else if(w_Overflow && i_Ready) begin
             r_IdxReg = r_IdxReg - (BUS_WIDTH-DELTA);                            // no shift due to information loss --> step back
         end
     end
@@ -142,8 +152,8 @@ module vec_cat
     always @ (posedge clk)
     begin
         if(!rstn) begin
-            r_IDCntr <= {VEC_ID_WIDTH{1'b1}};
-        end else if(i_Valid && (r_State == PAD)) begin
+            r_IDCntr <= 0;
+        end else if(w_DoShift && (w_State == PAD)) begin
             r_IDCntr <= r_IDCntr + 1;
         end
     end
@@ -153,10 +163,10 @@ module vec_cat
     // SELECT OUTPUT
     // --> select the correct output from the r_OutVectorArray register array
 
-    assign o_Vector = (r_State == FULL) ? w_PermArray[r_IdxReg] : {w_PermArray[r_IdxReg][BUS_WIDTH-1:DELTA], {DELTA{1'b0}}};
+    assign o_Vector = (w_State == FULL) ? w_PermArray[r_IdxReg] : {w_PermArray[r_IdxReg][BUS_WIDTH-1:DELTA], {DELTA{1'b0}}};
     assign o_VecID  = r_IDCntr;
     assign o_Valid  = r_ValidShr[0];
-    assign o_Read   = i_Valid && ~w_Overflow;
+    assign o_Read   = w_DoShift && ~w_Overflow;
     assign o_Last   = r_LastShr[0];
 
     
